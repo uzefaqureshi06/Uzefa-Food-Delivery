8be23cdfde5c0cefe06218062f166a4e6b432811
uzefaqureshi06 <uzefaqureshi06@gmail.com>
2026-05-24 06:06:14 +0530

    fix vercel deployment

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ import cors from "cors";
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoutes.js";
 import userRouter from "./routes/userRoutes.js";
-import "dotenv/config";
 import cartRouter from "./routes/cartRoutes.js";
 import orderRouter from "./routes/orderRoutes.js";
+import "dotenv/config";
+
 const app = express();
-const port = 4000;
 
 // middleware
 app.use(express.json());
@@ -18,15 +18,17 @@ connectDB();
 
 // static folder
 app.use("/images", express.static("uploads"));
+
 // api routes
 app.use("/api/food", foodRouter);
 app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
-app.use("/api/order",orderRouter); 
+app.use("/api/order", orderRouter);
+
 app.get("/", (req, res) => {
   res.send("API Working");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+// REMOVE app.listen()
+
+export default app;
