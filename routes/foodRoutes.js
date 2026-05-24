51201cb0eb65b2c14397c355dbcf45e04a32145b
uzefaqureshi06 <uzefaqureshi06@gmail.com>
2026-05-24 06:20:33 +0530

    Fix multer uploads

--- routes/foodRoutes.js
+++ routes/foodRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import multer from "multer";
+import fs from "fs";
 
 import {
   addFood,
@@ -9,15 +10,23 @@ import {
 
 const foodRouter = express.Router();
 
+// create uploads folder if it doesn't exist
+if (!fs.existsSync("uploads")) {
+  fs.mkdirSync("uploads");
+}
+
 // image storage engine
 const storage = multer.diskStorage({
-  destination: "uploads",
-  filename: (req, file, cb) => {
-    return cb(null, `${Date.now()}-${file.originalname}`);
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}-${file.originalname}`);
   },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage });
 
 // routes
 foodRouter.post("/add", upload.single("image"), addFood);
